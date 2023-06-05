7bd4bb1a90ab1ac6bed15dde2ea0052db714d2e9
daria melinte <dariamelinte2003@gmail.com>
2023-06-05 18:24:01 +0300

    forgot password

--- grocery/src/main/java/com/example/grocery/api/auth/AuthController.java
+++ grocery/src/main/java/com/example/grocery/api/auth/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.grocery.api.auth;
 
+import com.example.grocery.api.auth.forgotPassword.ForgotPasswordDTO;
 import com.example.grocery.api.auth.login.LoginDTO;
 import com.example.grocery.api.auth.register.RegisterDTO;
 import com.example.grocery.api.user.User;
@@ -30,6 +31,12 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<Response<User>> logout(@RequestHeader("Authorization") String bearerToken) {
         Response<User> response = authService.logout(bearerToken);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<Response<User>> forgotPassword(@RequestBody ForgotPasswordDTO forgotPasswordDTO) {
+        Response<User> response = authService.forgotPassword(forgotPasswordDTO);
         return new ResponseEntity<>(response, response.getStatus());
     }
 }
