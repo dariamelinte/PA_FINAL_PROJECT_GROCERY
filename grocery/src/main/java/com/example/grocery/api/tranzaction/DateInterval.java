66fa46dfcc53b7fb3dc177fd60693645552da85c
Vamanu Petru <petrisor_vmy@yahoo.com>
2023-06-07 13:24:05 +0300

    Tranzaction additional queries done

---
+++ grocery/src/main/java/com/example/grocery/api/tranzaction/DateInterval.java
@@ -0,0 +1,11 @@
+package com.example.grocery.api.tranzaction;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class DateInterval {
+    private Date startDate;
+    private Date endDate;
+}
--- grocery/src/main/java/com/example/grocery/api/tranzaction/TranzactionRepository.java
+++ grocery/src/main/java/com/example/grocery/api/tranzaction/TranzactionRepository.java
@@ -1,9 +1,11 @@
 package com.example.grocery.api.tranzaction;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +13,12 @@ import java.util.Optional;
 public interface TranzactionRepository extends MongoRepository<Tranzaction, String> {
     @Query("{ 'productGroceryId' : ObjectId('?0')}")
     Optional <List<Tranzaction>> findByProductGroceryId(String productGroceryId);
+
+
+    @Query("{ userId: ObjectId('?0'),  createdAt: {$gte: ?1, $lte: ?2 }}")
+
+    List<Tranzaction> findByDateAndUser(String userid, Date startDate, Date endDate);
+
+    @Query("{ productGroceryId: ObjectId('?0'),  createdAt: {$gte: ?1, $lte: ?2 }}")
+    List<Tranzaction> findByDateAndGrocery(String productGroceries, Date startDate, Date endDate);
 }
